17ba828c5f0f802ee9bf94dc09d372d235d88d7d
Seandon Mooy <admin@erulabs.com>
2013-12-12 15:36:04 -0600

    burst.js pretty much complete - added options to config.js

--- burst.js
+++ burst.js
@@ -1,15 +1,118 @@
 #!/usr/bin/env node
 // Include racks.js
 var RacksJS = require('./racksjs/racks.js'),
-    config = require('./config.js');
+    config = require('./config.js'),
+    fs = require('fs');
 
 new RacksJS({
     username: config.username,
     apiKey: config.apikey,
     verbosity: config.verbosity
 }, function (rack) {
+    var targetLB = false,
+        newServer = false,
+        serverName = process.argv[2];
     if (rack.error) {
         console.log(rack.error);
         return false;
     }
+    if (serverName === undefined) {
+        console.log('No servername given - usage: ./burst.js <servername>');
+        return false;
+    }
+    findOrCreateTargetLB();
+
+    function findOrCreateTargetLB () {
+        rack.cloudLoadBalancers.loadBalancers.all(function (loadBalancers) {
+            loadBalancers.forEach(function (lb) {
+                if (lb.name === config.loadBalancerName) {
+                    targetLB = lb;
+                    findOrCreateServer();
+                }
+            });
+            if (!targetLB) {
+                console.log('No load balancer named "' + config.loadBalancerName + '" was found - creating');
+                rack.cloudLoadBalancers.loadBalancers.new({
+                    'name': config.loadBalancerName
+                }, function (loadbalancer) {
+                    var progressCheck = function () {
+                        setTimeout(function () {  
+                            loadbalancer.details(function (details) {
+                                if (details.status === 'BUILD') {
+                                    console.log('Load balancer building...');
+                                    progressCheck();
+                                } else if (details.status === 'ACTIVE') {
+                                    console.log('load balancer build complete!');
+                                    targetLB = loadbalancer;
+                                    targetLB.loadedDetails = details;
+                                    findOrCreateServer();
+                                } else {
+                                    console.log('Some error occured!', details.status);
+                                }
+                            });
+                        }, 15000);
+                    };
+                    progressCheck();
+                });
+            }
+        });
+    };
+    function findOrCreateServer () {
+        rack.cloudServersOpenStack.servers.all(function (servers) {
+            servers.forEach(function (server) {
+                if (server.name === serverName) {
+                    console.log('It appears a server by that name already exists');
+                    newServer = server;
+                }
+            });
+            if (newServer) {
+                return false;
+            }
+            rack.cloudServersOpenStack.servers.new({
+                "name": serverName,
+                "imageRef": config.serverBaseImage,
+                "flavorRef": config.serverFlavor
+            }, function (server) {
+                var progressCheck = function () {
+                    setTimeout(function () {  
+                        server.details(function (details) {
+                            if (details.progress < 100) {
+                                console.log('Server "' + serverName + '" building -', details.progress + '%');
+                                progressCheck();
+                            } else {
+                                newServer = server;
+                                newServer.loadedDetails = details;
+                                addServerToAnsibleConfig(newServer);
+                                console.log('build complete! root pw:', server.adminPass, 'server id:', server.id);
+
+                            }
+                        });
+                    }, 15000);
+                };
+                progressCheck();
+            });
+        });
+    };
+    function addServerToAnsibleConfig (server) {
+        var pubIP = false,
+            serverString;
+        server.loadedDetails.addresses['public'].forEach(function (addr) {
+            if (addr.version === 4) {
+                pubIP = addr.addr;
+            }
+        });
+        if (pubIP) {
+            serverString = serverName + "\tansible_ssh_host=" + pubIP + "\tansible_ssh_user=root\tansible_ssh_pass=" + server.adminPass + "\n";
+            fs.appendFile(config.ansibleInventory, serverString, function (err) {
+                if (err) {
+                    console.log('failed to write to ansibleInventory, file:', config.ansibleInventory, 'error:', err);
+                } else {
+                    console.log('New server', serverName, 'is complete! Run ./configure.sh to ensure all hosts are configured correctly!');
+                }
+            });
+        } else {
+            console.log('couldnt find the public addr of the new server.');
+            console.log('Perhaps a code issue with burst.js - or perhaps the servers in fact dont have public ipv4 addrs');
+        }
+    };
 });
--- config.js
+++ config.js
@@ -1,5 +1,18 @@
 module.exports = {
-	username: 'foobar',
-	apikey: 'something',
-	verbosity: 5
+	// Rackspace API Authentication information
+	username: 'seandon',
+	apikey: '295f5d723df3d3a394ed33549e475435',
+	// The name of our load balancer - if it doesn't exist, it will be created by burst.js
+	loadBalancerName: 'devops_prod_LB',
+	// The base image used for creating servers - this is CentOS 6.4
+	serverBaseImage: 'f70ed7c7-b42e-4d77-83d8-40fa29825b85',
+	// The flavor to use (server specs). '2' is a 512mb Next-Gen server.
+	// For real production mode, we'd probably want to use 'performance1-2', which is a 2gb performance server
+	serverFlavor: '2',
+	// RacksJS verbosity level
+	// set to zero to only see script-level messaging
+	// set to 5 to see full request/reply chain for every API call
+	verbosity: 5,
+	// name of the Ansible inventory file
+	ansibleInventory: 'hosts'
 };
